49196a7b81b4225c058c6cd1e57aa2d37142c30a
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-09-26 15:39:49 +0800

    logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,13 +17,11 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="images/logo.svg">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>
-        {{ env("APP_NAME") }} Dashboard
-    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}" />
+    <link rel="icon" type="image/png" href="images/logo.svg" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>YSL</title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Nucleo Icons -->
@@ -38,32 +36,29 @@
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css"
-        href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css">
+        href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css" />
 
     <!-- CSS Files -->
     <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
-    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
-
 </head>
 
-<body class="g-sidenav-show   bg-gray-100">
+<body class="g-sidenav-show bg-gray-100">
     <div class="min-height-300 bg-primary position-absolute w-100"></div>
     <aside
-        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
+        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4"
         id="sidenav-main">
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
                 aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href="" target="_blank">
-                <img src="{{ asset('images/logo-large.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-
+                <img src="{{ asset('images/logo2.png') }}" class="navbar-brand-img h-100" alt="main_logo" />
             </a>
         </div>
-        <hr class="horizontal dark mt-0">
-        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+        <hr class="horizontal dark mt-0" />
+        <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
             <ul class="navbar-nav">
-                <li class="nav-item ">
+                <li class="nav-item">
                     <a class="nav-link {{ request()->routeIs('admin') ? 'active' : '' }}" href="{{ route('admin') }}">
                         <div
                             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -72,7 +67,7 @@
                         <span class="nav-link-text ms-1">Dashboard</span>
                     </a>
                 </li>
-                <li class="nav-item ">
+                <li class="nav-item">
                     <a class="nav-link {{ request()->routeIs('users') ? 'active' : '' }}" href="{{ route('users') }}">
                         <div
                             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -81,8 +76,9 @@
                         <span class="nav-link-text ms-1">Users</span>
                     </a>
                 </li>
-                <li class="nav-item "></li>
-                    <a class="nav-link {{ request()->routeIs('scanner') ? 'active' : '' }}" href="{{ route('scanner') }}">
+                <li class="nav-item ">
+                    <a class="nav-link {{ request()->routeIs('scanner') ? 'active' : '' }}"
+                        href="{{ route('scanner') }}">
                         <div
                             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="ni ni-mobile-button text-warning text-sm opacity-10"></i>
@@ -90,39 +86,50 @@
                         <span class="nav-link-text ms-1">Scanner</span>
                     </a>
                 </li>
-
-
-
             </ul>
         </div>
     </aside>
-    <main class="main-content position-relative border-radius-lg ">
+    <main class="main-content position-relative border-radius-lg">
         <!-- Navbar -->
-        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
             data-scroll="false">
             <div class="container-fluid py-1 px-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white"
-                                href="javascript:;">Pages</a></li>
-                        <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+                        <li class="breadcrumb-item text-sm">
+                            <a class="opacity-5 text-white" href="javascript:;">Pages</a>
+                        </li>
+                        <li class="breadcrumb-item text-sm text-white active" aria-current="page">
+                            Dashboard
+                        </li>
                     </ol>
-                    <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+                    <h6 class="font-weight-bolder text-white mb-0">
+                        Dashboard
+                    </h6>
                 </nav>
-
+                <ul class="navbar-nav justify-content-end">
+                    <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                        <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                            <div class="sidenav-toggler-inner">
+                                <i class="sidenav-toggler-line bg-white"></i>
+                                <i class="sidenav-toggler-line bg-white"></i>
+                                <i class="sidenav-toggler-line bg-white"></i>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </nav>
         <!-- End Navbar -->
-        <div class="container-fluid ">
-            @yield('content')
-        </div>
+        <div class="container-fluid">@yield('content')</div>
 
-        <footer class="footer pt-3  ">
+        <footer class="footer pt-3">
             <div class="container-fluid">
                 <div class="row align-items-center justify-content-lg-between">
                     <div class="col-lg-6 mb-lg-0 mb-4">
                         <div class="copyright text-center text-sm text-muted text-lg-start">
-                            <a href="https://wowsome.com.my/" class="font-weight-bold" target="_blank">Wowsome</a> © Copyright 2024
+                            <a href="https://wowsome.com.my/" class="font-weight-bold" target="_blank">Wowsome</a>
+                            © Copyright 2024
                         </div>
                     </div>
                 </div>
@@ -130,14 +137,16 @@
         </footer>
     </main>
 
-
     <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
+        var win = navigator.platform.indexOf("Win") > -1;
+        if (win && document.querySelector("#sidenav-scrollbar")) {
             var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+                damping: "0.5",
+            };
+            Scrollbar.init(
+                document.querySelector("#sidenav-scrollbar"),
+                options
+            );
         }
     </script>
     <!-- Github buttons -->
@@ -146,8 +155,6 @@
     <script src="./assets/js/argon-dashboard.min.js?v=2.0.4"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-
 </body>
 
 </html>
