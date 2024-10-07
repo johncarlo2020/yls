306c4f0737d2b16e8a2231fddd59a7186111427b
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-10-07 18:17:52 +0800

    add safari padding

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,21 +1,22 @@
 <x-app-layout>
-    <div id="stationPage" class="station-page main main-bg">
+    <div id="stationPage" class="station-page main main-bg safari-padding">
         <div class="mb-3 branding-container">
             @include('components.branding')
         </div>
         <div id="mainContent" class="text-center text-content">
             <div class="content">
                 <h2 class="station-name">BORN TO BE ICONIC</h2>
-                <p class="tag-line">Start your immersive fragrance experience and discover the iconic scents of Libre Flowers & Flames and MYSLF Le Parfum.</p>
+                <p class="tag-line">Start your immersive fragrance experience and discover the iconic scents of Libre
+                    Flowers & Flames and MYSLF Le Parfum.</p>
             </div>
             <div class="station-img">
                 <img src="{{ asset('images/main.gif') }}" alt="" />
             </div>
 
             <div class="container w-50 mt-5">
-                    <a class="button" href="{{ route('dashboard') }}"> Discover Now</a>
-                </div>
+                <a class="button" href="{{ route('dashboard') }}"> Discover Now</a>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
